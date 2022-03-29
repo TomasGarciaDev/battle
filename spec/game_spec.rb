b8c93eb930f81f81e64c503afc837d3dea6c1edf
Tomas Garcia <tomasgarciadev@gmail.com>
2022-03-29 14:37:45 +0200

    Now playe instances are initialized with game instance, 'skinnier' controller

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,21 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
-  let(:player_1) { double :player }
-  let(:player_2) { double :player }
+  let(:player1) { double :player }
+  let(:player2) { double :player }
+  subject(:game) { described_class.new(player1, player2) }
   
   describe '#attack' do
     it 'damages the player' do
-      expect(player_2).to receive(:receive_damage)
-      game.attack(player_2)
+      expect(player2).to receive(:receive_damage)
+      game.attack(player2)
     end
+  end
+
+  describe 'instances of player' do
+    it 'takes 2 instances of player' do
+      expect(game.player1).to eq(player1)
+      expect(game.player1).to eq(player1)
+     end
   end
 end
